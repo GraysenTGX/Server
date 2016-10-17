61f0315f66a62161ee8f25998afd6e71ea21477c
GraysenTGX <tgx_seu@163.com>
2016-10-18 00:03:47 +0800

    add function define for IOCP class

--- serverIOCP/serverIOCP/IOCPServerbase.h
+++ serverIOCP/serverIOCP/IOCPServerbase.h
@@ -1,8 +1,35 @@
 #pragma once
+
+#include <WinSock2.h>
+#include <boost/thread.hpp>
+
+#define LPFN_ACCEPTEX LPVOID
+
+
 class CIOCPServerbase
 {
 public:
 	CIOCPServerbase(void);
 	virtual ~CIOCPServerbase(void);
+
+public:
+	DWORD GetCPUNum();
+	BOOL SocketEnvInitialize();
+	BOOL CreateNewCompletionPort(DWORD WorkThreadNum);
+	BOOL Associate2CompletionHandle(HANDLE &completionPort, HANDLE &newDevice, DWORD &hCmpletionKey);
+	void *GetAcceptEXFuncAddress();
+	HANDLE CreateNewHandle();
+	HANDLE GetIOCPortHandle();
+	BOOL run();
+
+
+public:
+	virtual void RecvDataProc(HANDLE &device, HANDLE &IOComloetionKey);
+	virtual void SendDataProc(HANDLE &device, HANDLE &IOComloetionKey);
+
+private:
+	HANDLE IOCPort;
+	LPFN_ACCEPTEX m_AcceptEXAddress;
+
 };
 
